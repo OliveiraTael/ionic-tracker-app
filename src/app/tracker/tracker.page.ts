5fc8286fe5f4b39c43abaeebc2c7c6e35e2c7711
6784 <6784@ait-sydney.net.au>
2019-11-13 14:57:42 +1100

    week 2

--- src/app/tracker/tracker.page.ts
+++ src/app/tracker/tracker.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Validators, FormBuilder, FormGroup, Form} from '@angular/forms';
+import {timer, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-tracker',
@@ -8,18 +10,40 @@ import { Component, OnInit } from '@angular/core';
 export class TrackerPage implements OnInit {
 
   started:Boolean = false;
+  taskForm:FormGroup;
+  startTime:number;
+  stopTime:number;
 
-  constructor() { }
+  timerSub:Subscription;
+  time:number;
+
+  constructor(
+    private formBuilder:FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.taskForm = this.formBuilder.group({
+      name:['', [Validators.required, Validators.minLength(3)]]
+    });
   }
 
   start(){
     this.started = true;
+    this.startTime = new Date().getTime();
+
+    const tm = timer(0, 1000);
+    this.timerSub = tm.subscribe(val => this.time = val);
+
+    console.log(this.startTime);
   }
 
   stop(){
     this.started = false;
+    this.stopTime = new Date().getTime();
+
+    this.timerSub.unsubscribe();
+
+    console.log(this.stopTime);
   }
 
 }
